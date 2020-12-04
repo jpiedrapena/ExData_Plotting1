80df2c73c6978bc8a32645697c407c938c860f62
jpiedrapena <71022447+jpiedrapena@users.noreply.github.com>
2020-12-04 15:45:00 +0100

    Plot 2

---
+++ Plot2.R
@@ -0,0 +1,17 @@
+#install.packages("data.table")
+library(data.table)
+
+#Read data set
+data <- fread(input = "household_power_consumption.txt", na.strings="?")
+
+data[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
+
+# Filter Dates for 2007-02-01 and 2007-02-02
+data <- data[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
+
+png("plot2.png", width=480, height=480)
+
+## Plot 2
+plot(x = data$dateTime, y = data$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+
+dev.off()
